9b1395baff4caf4e1857fd3a73886fe7462d5b82
charles delannoy <charles.delannoy@hei.fr>
2021-04-25 22:11:02 +0200

    searchBar hooked

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,29 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class SearchBar extends React.Component {
-  state = { term: '' };
+const SearchBar = ({ onSubmit }) => {
+  const [term, setTerm] = useState('');
 
-  onFormSubmit = (event) => {
+  const onFormSubmit = (event) => {
     event.preventDefault();
-    this.props.onSubmit(this.state.term);
-  }
+    onSubmit(term);
+  };
 
-  render() {
-    return (
-      <div className="ui segment">
-        <form className="ui form" onSubmit={this.onFormSubmit}>
-          <div className="field">
-            <label>Search videos</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={e => this.setState({term: e.target.value})}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="ui segment">
+      <form className="ui form" onSubmit={onFormSubmit}>
+        <div className="field">
+          <label>Search videos</label>
+          <input
+            type="text"
+            value={term}
+            onChange={e => setTerm(e.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
